858c588ee574a05ca428cfe2674daa3ae3699bff
脇田　慎平 <ymnkknt3@gmail.com>
2020-10-21 08:54:01 +0900

    [ add ] githubログイン機能実装テスト

--- config/services.php
+++ config/services.php
@@ -35,4 +35,9 @@ return [
         'redirect' => env('TWITTER_CLIENT_CALLBACK')
     ],
 
+    'github' => [
+        'client_id'     => env('GITHUB_CLIENT_ID'),
+        'client_secret' => env('GITHUB_CLIENT_SECRET'),
+        'redirect'      => env('GITHUB_URL'),
+    ],
 ];
