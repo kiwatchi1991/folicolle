399bf1e17d343f38c01e449b301e5ae202509778
脇田　慎平 <ymnkknt3@gmail.com>
2020-10-23 01:10:55 +0900

    [ add ] ユーザーモデルの$fillableに追加

--- app/User.php
+++ app/User.php
@@ -18,7 +18,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'twitter_id'
+        'name', 'email', 'password', 'twitter_id', 'github_id', 'google_id'
     ];
 
     /**
