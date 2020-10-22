8a1a1a18145b8a45b6108134f283685d8e431c43
脇田　慎平 <ymnkknt3@gmail.com>
2020-10-22 21:19:07 +0900

    [ Update ] cors対策

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -47,6 +47,11 @@ class Kernel extends HttpKernel
             'throttle:60,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
+        'session' => [
+            \App\Http\Middleware\EncryptCookies::class,
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+        ],
     ];
 
     /**
